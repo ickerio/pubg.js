6a2026bbae32166a27559d37b89c4608b5a3acbe
Harry Ickeringill <harry.ickeringill@gmail.com>
2017-10-03 17:30:25 +0800

    Better travis testing

--- test/index.js
+++ test/index.js
@@ -2,12 +2,21 @@ const pubgClient = require('../');
 
 const client = new pubgClient(process.env.PUBG_KEY);
 
-client.getProfile('ickerio').then(profile => {
+console.log(`Testing version ${client.version} of pubg.js`);
 
+// Test 1: getProfile
+client.getProfile('ickerio')
+    .then(() => console.log('Test 1: Success'));
+
+// Test 2: getStats of profile
+client.getProfile('ickerio').then(profile => {
     profile.getStats({
         region: 'oc',
         season: '2017-pre4',
         match: 'solo'
     }).getItem('RoundsPlayed');
+}).then(() => console.log('Test 2: Successs'));
 
-}).catch(console.error);
+// Test 3: getAccount
+client.getAccount('76561198114629752')
+    .then(() => console.log('Test 3: Success'));
